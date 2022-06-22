0f2fb027638c50920acbc2f579a0ba819bb052c9
Oleh Tsiupak <tsyupak98@gmail.com>
2022-06-22 14:52:31 +0300

    install Docker and run nginx image

--- main.tf
+++ main.tf
@@ -121,6 +121,8 @@ resource "aws_instance" "myapp-server" {
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
 
+    user_data = file("entry-script.sh")
+
     tags = {
         Name = "${var.env_prefix}-server"
     }
