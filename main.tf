386c154585028291f4a33adae3a34b4c881a7e06
Oleh Tsiupak <tsyupak98@gmail.com>
2022-06-24 14:10:45 +0300

    add webserver module

--- main.tf
+++ main.tf
@@ -18,65 +18,14 @@ module "myapp-subnet" {
     default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
 
-resource "aws_default_security_group" "default-sg" {
+module "myapp-server" {
+    source = "./modules/webserver"
     vpc_id = aws_vpc.myapp-vpc.id
-  
-     ingress {
-       from_port   = 22
-       to_port     = 22
-       protocol    = "tcp"
-       cidr_blocks = [var.my_ip]
-     }
-     ingress {
-       from_port   = 8080
-       to_port     = 8080
-       protocol    = "tcp"
-       cidr_blocks = ["0.0.0.0/0"]
-     }
-     egress {
-       from_port       = 0
-       to_port         = 0
-       protocol        = "-1"
-       cidr_blocks     = ["0.0.0.0/0"]
-       prefix_list_ids = []
-     }
-     tags = {
-      Name = "${var.env_prefix}-default-sg"
-    }
-}
-
-data "aws_ami" "latest-amazon-linux-image" {
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-    }
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-}
-
-resource "aws_key_pair" "ssh-key" {
-    key_name = "server-key"
-    public_key = var.my_public_key
-}
-
-resource "aws_instance" "myapp-server" {
-    ami = data.aws_ami.latest-amazon-linux-image.id
+    my_ip = var.my_ip
+    env_prefix = var.env_prefix
+    image_name = var.image_name
+    my_public_key = var.my_public_key
     instance_type = var.instance_type
-
     subnet_id = module.myapp-subnet.subnet.id
-    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-    availability_zone = var.avail_zone
-
-    associate_public_ip_address = true
-    key_name = aws_key_pair.ssh-key.key_name
-
-    user_data = file("entry-script.sh")
-
-    tags = {
-        Name = "${var.env_prefix}-server"
-    }
-}
+    avail_zone = var.avail_zone
+} 
--- variables.tf
+++ variables.tf
@@ -5,3 +5,4 @@ variable env_prefix {}
 variable my_ip {}
 variable instance_type {}
 variable my_public_key {}
+variable image_name {}
