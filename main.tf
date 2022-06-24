77b9944abccd00704f660825b66bbde3e93a3ed0
Oleh Tsiupak <tsyupak98@gmail.com>
2022-06-24 13:19:01 +0300

    add subnet module

--- main.tf
+++ main.tf
@@ -2,14 +2,6 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable my_ip {}
-variable instance_type {}
-variable my_public_key {}
-
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
@@ -17,48 +9,13 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-  tags = {
-      Name = "${var.env_prefix}-subnet-1"
-  }
-}
-
-/*resource "aws_route_table" "myapp-route-table" {
-   vpc_id = aws_vpc.myapp-vpc.id
-
-   route {
-     cidr_block = "0.0.0.0/0"
-     gateway_id = aws_internet_gateway.myapp-igw.id
-   }
-   tags = {
-     Name = "${var.env_prefix}-rtb"
-   }
-}*/
-
-resource "aws_internet_gateway" "myapp-igw" {
-	vpc_id = aws_vpc.myapp-vpc.id
-    tags = {
-     Name = "${var.env_prefix}-igw"
-   }
-}
-
-/*resource "aws_route_table_association" "a-rtb-subnet" {
-  subnet_id      = aws_subnet.myapp-subnet-1.id
-  route_table_id = aws_route_table.myapp-route-table.id
-}*/
-
-resource "aws_default_route_table" "main-rtb" {
+module "myapp-subnet" {
+    source = "./modules/subnet"
+    subnet_cidr_block = var.subnet_cidr_block
+    avail_zone = var.avail_zone
+    env_prefix = var.env_prefix
+    vpc_id = aws_vpc.myapp-vpc.id
     default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-    route {
-     cidr_block = "0.0.0.0/0"
-     gateway_id = aws_internet_gateway.myapp-igw.id
-   }
-   tags = {
-     Name = "${var.env_prefix}-main-rtb"
-   }
 }
 
 resource "aws_default_security_group" "default-sg" {
@@ -101,10 +58,6 @@ data "aws_ami" "latest-amazon-linux-image" {
     }
 }
 
-output "ec2_public_ip" {
-    value = aws_instance.myapp-server.public_ip
-}
-
 resource "aws_key_pair" "ssh-key" {
     key_name = "server-key"
     public_key = var.my_public_key
@@ -114,7 +67,7 @@ resource "aws_instance" "myapp-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = var.instance_type
 
-    subnet_id = aws_subnet.myapp-subnet-1.id
+    subnet_id = module.myapp-subnet.subnet.id
     vpc_security_group_ids = [aws_default_security_group.default-sg.id]
     availability_zone = var.avail_zone
 
